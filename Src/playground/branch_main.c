a7c593eefffe7a9fed0bd6ca58a33c7689d1044e
FofLof <ethan_see@hotmail.com>
2024-04-09 00:26:33 -0700

    Exercise 1

--- Src/playground/branch_main.c
+++ Src/playground/branch_main.c
@@ -6,10 +6,33 @@
 
 void branch_main() {
 	// space for initializations
-
+	buttons_init();
+	printer_init();
+	led_init();
+	uint64_t initTime = getSysTime();
+	uint64_t changeInTime = 0;
+	bool currentState = false;
+	led_d1(currentState);
 
 	// infinite loop
 	while (1) {
+		changeInTime = getSysTime() - initTime;
+
+		if (get_buttonStatus_0()) {
+			printMsg("Button 0 ON\r\n");
+		}
+
+		if (get_buttonStatus_1()) {
+			printMsg("Button 1 ON\r\n");
+		}
+
+		if (changeInTime > 1000) {
+			changeInTime = 0;
+			currentState = !currentState;
+			led_d1(currentState);
+
+		}
+
 
 	}
 
